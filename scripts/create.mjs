092af1c29dd3003294d449f4ab3524dc328ee41e
Csmally <1130180692@qq.com>
2023-11-23 00:31:00 +0800

    feat: 优化创建文件脚本

--- scripts/create.mjs
+++ scripts/create.mjs
@@ -5,9 +5,20 @@ import { execSync } from 'child_process';
 // 获取命令行参数
 const args = process.argv.slice(2);
 // 根据输入参数执行不同的命令
-if (args.length === 1) {
+const length = args.length;
+let command;
+if (length === 1 || length === 2) {
   const resourceName = args[0];
-  const command = `nest g res ${resourceName} --no-spec`;
+  if (length === 1) {
+    command = `nest g res ${resourceName} --no-spec`;
+  } else {
+    if (args[1] === 'sprc') {
+      command = `nest g res ${resourceName}`;
+    } else {
+      console.error('脚本输入有误，执行失败');
+      process.exit(1);
+    }
+  }
   // 执行命令
   try {
     execSync(command, { stdio: 'inherit' });
@@ -16,6 +27,6 @@ if (args.length === 1) {
     process.exit(1);
   }
 } else {
-  console.error('执行失败');
+  console.error('脚本参数数量错误，执行失败');
   process.exit(1);
 }
