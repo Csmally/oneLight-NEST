8ab91b69c85362a92b4bb950324b57b2743a7175
Csmally <1130180692@qq.com>
2023-11-23 04:10:01 +0800

    feat: 解决安卓本地请求case

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseFormat } from './common/responseFormat';
 import { ErrorFilter } from './common/errorFilter';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('localhost-key.pem'),
-    cert: fs.readFileSync('localhost.pem'),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ErrorFilter());
   app.useGlobalInterceptors(new ResponseFormat());
   await app.listen(3000);
